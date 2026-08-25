9562e9f92bfa04a274c4b4b5a490bad4b14927d8
frogsonthecombos <112841049+frogsonthecombos@users.noreply.github.com>
2026-08-24 23:54:01 -0500

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5735920394950369"
+     crossorigin="anonymous"></script>
+<!-- Classroom -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5735920394950369"
+     data-ad-slot="5886119555"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
